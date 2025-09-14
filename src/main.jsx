33a9e0fd54a714f37e77d251f5b171bca6e186f9
ash1198 <ash.ranjan09@gmail.com>
2025-09-14 14:17:33 +0530

    changed project name to sde-qna-handbook

--- src/main.jsx
+++ src/main.jsx
@@ -5,7 +5,7 @@ import { BrowserRouter } from 'react-router-dom'
 
 createRoot(document.getElementById('root')).render(
     <>
-        <BrowserRouter basename='/qna-handbook'>
+        <BrowserRouter basename='/sde-qna-handbook'>
             <App />
         </BrowserRouter>
     </>,
